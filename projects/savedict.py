c1d7655b4dad4a9308c53fa0d7aac246f35a5866
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-06-03 02:06:47 +0000

    dictionary project

--- projects/savedict.py
+++ projects/savedict.py
@@ -1,8 +1,14 @@
 # save function - saves dictionary into a file 
 # loads - saves new information into given file 
+import pickle 
 
-def save(dict, outputpath):
-    pass
+def save_dict(dict, outputpath):
+    with open(outputpath, 'wb') as saveinput:
+        pickle.dump(dict, saveinput)
+        
 
-def load(filepath):
-    pass
+def load_dict(filepath):
+    with open(filepath, 'rb') as savedinput:
+        opendict = pickle.load(savedinput)
+    return opendict 
+
