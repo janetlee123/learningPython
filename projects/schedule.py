fc153a9d76b57d487eee382e9cfd9e22ab2148bf
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-06-04 00:34:03 +0000

    try schedule function

--- projects/schedule.py
+++ projects/schedule.py
@@ -1,11 +1,12 @@
 # imputs event time, function, any number of arguments 
 # example of call to schedule_function
 # schedule_function(time.time()+1, print, 'Howdy!')
-import schedule
+import sched
 import time 
 
 def schedule_function( time, func, *args ):
-    schedule.every(time,func,*args)
+    scheduler = sched.scheduler(time.time(), time.sleep() )
+    scheduler.enter(time,func,*args)
 
 
 
