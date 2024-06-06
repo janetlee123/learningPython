b01c6dbc551b5192cacc27b1686184e5556c7bb5
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-06-06 16:27:37 +0000

    dice probability file

--- send_email.py
+++ send_email.py
@@ -1,15 +1,20 @@
 # input : receiver email address, subject line, message body 
 # function : sends the email 
 # References used: 
-# https://developers.google.com/gmail/api/quickstart/python 
-# https://realpython.com/python-send-email/ 
+# https://docs.python.org/3/library/email.examples.html
+import smtplib 
 
+from email.message import EmailMessage
 
 def send_email(address, subject, message):
-    
-    #Exceptions
-    if not (isinstance(address,str) or isinstance(subject,str) 
-            or isinstance(message,str)):
-        raise TypeError(f"address, subject and message must be a string ")
+    my_address = 'learningpython351@gmail.com'
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = subject 
+    msg['To'] = address 
+    msg['From'] = my_address 
 
-    #
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+    
