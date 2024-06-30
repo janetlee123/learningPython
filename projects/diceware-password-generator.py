79320713fcb8cf78577d2d4c539ba7381bd29a74
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-06-30 00:01:19 +0000

    finished diceware project

--- projects/diceware-password-generator.py
+++ projects/diceware-password-generator.py
@@ -1,16 +1,29 @@
 #creates a list of 6 random words using diceware to be used as password 
 # copied MIT wordlist.txt from https://gist.github.com/a6994f236ac07ebb45e6eca7a7312e9e.git 
+
+#TO USE: 
+# enter the file path of the wordlist.txt into the passwordgenerator
+
 import random 
-wordlist=[]
+listofwords=[]
+password=[]
+i=1 
 
-def passwordgenerator():
-    with open(wordlist.txt, "r") as f:
+def passwordgenerator(filepath):
+    with open(filepath, "r") as f:
         for line in f: 
-            wordlist.extend(line.split()) 
+            if len(line) >= 4: 
+                listofwords.extend(line.split()) 
 
-    print(wordlist[0])
+    for i in range(1,6):
+        randint = random.randint(1,len(listofwords))
+        password.append(listofwords[randint])
+        i=i+1
 
-passwordgenerator()
+    print(password)    
+
+# checking to see if it works 
+passwordgenerator("/workspaces/learningPython/projects/wordlist.txt")
 
 
 
