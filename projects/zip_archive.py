5f06d698034a9c808c9f2d97e7561509c439b4cd
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-07-05 02:45:28 +0000

    zip files archive project

---
+++ projects/zip_archive.py
@@ -0,0 +1,24 @@
+#To zip a list of file extentions from a directory to an outfile file path 
+# https://www.tutorialspoint.com/How-to-create-a-zip-file-using-Python
+
+import os 
+from zipfile import ZipFile
+
+def zip_files(directorypath, listoffiles,outputfilepath):
+    #creating a zipfile object named ZipFile
+    with ZipFile(outputfilepath, 'w') as zip_object:
+        # using os.walk to go through all files in directory
+        for folder_name, sub_folders, file_names in os.walk(directorypath):
+            #for each file under their respective folders, make new folder
+            for filename in file_names: 
+                #Creating filepath for files in directory 
+                file_path = os.path.join(folder_name,filename)
+                
+                # adding files to zip file 
+                zip_object.write(file_path, os.path.basename(file_path)) 
+    
+    if os.path.exists(outputfilepath):
+        print("ZIP file created")
+    else: 
+        print("ZIP file not created")
+        
