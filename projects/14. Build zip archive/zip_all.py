9c40d45dc943ac08241e450298f2d67b4e22a960
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-07-06 01:59:34 +0000

    finished zip archive project

--- projects/14. Build zip archive/zip_all.py
+++ projects/14. Build zip archive/zip_all.py
@@ -4,22 +4,19 @@
 import os 
 from zipfile import ZipFile
 
-def zip_files(directorypath, list_of_file_extensions,outputfilepath):
+def zip_files(directory_name, list_of_file_extensions,output_filepath):
     #creating a zipfile object named ZipFile
-    with ZipFile(outputfilepath, 'w') as zip_object:
+    with ZipFile(output_filepath, 'w') as zip_object:
         # using os.walk to go through all files in directory
-        for folder_name, sub_folders, file_names in os.walk(directorypath):
+        for folder_name, _, extension in os.walk(directory_name):
+            real_path = os.path.relpath(folder_name, directory_name)
             #for each file under their respective folders, make new folder
             for filename in file_names:
-                for extensions in list_of_file_extensions:
-                    if filename.endswith(extensions):  
-                    #Creating filepath for files in fileextention in directory 
-                        file_path = os.path.join(folder_name,filename)
-                        
-                        # adding files to zip file 
-                        zip_object.write(file_path, os.path.basename(file_path)) 
+                extension, file_names = os.path.splitext(filename)
+                if extension.lower() in list_of_file_extensions:
+                    zip_object.write(os.path.join(folder_name,file),arcname=os.path.join(real_path,file))
     
-    if os.path.exists(outputfilepath):
+    if os.path.exists(output_filepath):
         print("ZIP file created")
     else: 
         print("ZIP file not created")
