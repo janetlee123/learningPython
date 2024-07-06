487892386101d77a330460e81083d46faf098b2a
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-07-06 01:31:36 +0000

    trying something new

--- projects/14. Build zip archive/zip_all.py
+++ projects/14. Build zip archive/zip_all.py
@@ -11,12 +11,13 @@ def zip_files(directorypath, list_of_file_extensions,outputfilepath):
         for folder_name, sub_folders, file_names in os.walk(directorypath):
             #for each file under their respective folders, make new folder
             for filename in file_names:
-                if filename in os.path.basename(list_of_file_extensions):  
+                for extensions in list_of_file_extensions:
+                    if filename.endswith(extensions):  
                     #Creating filepath for files in fileextention in directory 
-                    file_path = os.path.join(folder_name,filename)
-                    
-                    # adding files to zip file 
-                    zip_object.write(file_path, os.path.basename(file_path)) 
+                        file_path = os.path.join(folder_name,filename)
+                        
+                        # adding files to zip file 
+                        zip_object.write(file_path, os.path.basename(file_path)) 
     
     if os.path.exists(outputfilepath):
         print("ZIP file created")
